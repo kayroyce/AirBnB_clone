e6705e306c51651af92daa0cba880d33fb6d37f4
kayroyce <102330728+kayroyce@users.noreply.github.com>
2022-09-06 21:40:00 +0100

    test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Defines unittests for models/city.py.
-Unittest classes:
+""" Defines unittests for models/city.py.
+    Unittest classes:
     TestCity_instantiation
     TestCity_save
     TestCity_to_dict
